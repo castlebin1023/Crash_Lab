dbb67d379378b7135d5a7274a157a3790d2b04f0
Sungbin_cho <csb011023@hanyang.ac.kr>
2024-10-28 14:46:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-import crashstt as cr
+import crash_lang as cr
+import time
+import threading
+import openai
+import os
+from openai import OpenAI
 
-cr.find_mic()
+def ask_gpt(prompt):
+    try:
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt},
+        ]
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        print(f"Can't access API;{e}")
+        return None
+    
+micindex = 6 
+client = OpenAI()
+
+question = cr.google_free(micindex)
+gpt_response = ask_gpt(question)
+print("GPT Answer:", gpt_response)
+cr.speak(gpt_response,"ko")

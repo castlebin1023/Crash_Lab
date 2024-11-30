9945546e1ac191db9e081297ae595fe1323c9434
Sungbin_cho <csb011023@hanyang.ac.kr>
2024-11-30 20:55:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import crash_lang as cr
+import usb.core #for mic tuning
+import usb.util #for mic tuning
 import time
-import threading
-import openai
-import os
-from openai import OpenAI
 
-def ask_gpt(prompt):
-    try:
-        completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": prompt},
-        ]
-        )
-        return completion.choices[0].message.content
-    except Exception as e:
-        print(f"Can't access API;{e}")
-        return None
+dev = usb.core.find(idVendor=0x2886, idProduct=0x0018) #for mic tuning
+keywords = ["민준","안내"] 
+mic_index = 5 
+speech_recognizer = cr.BackgroundSpeechRecognizer(mic_index,keywords)
+touch_toggle = False
+
+
+try:
+    cr.audio_initialize()
+    cr.set_parm(dev)
+    print("Starting background speech recognition...")
+    speech_recognizer.start_listening()
+    print("Listening in the background")
     
-micindex = 6 
-client = OpenAI()
+    while True:
+        if speech_recognizer.trigger or touch_toggle :
+            # print("Start Guide!")
+            if speech_recognizer.recognized_text :
+                response =cr.ask_gpt(speech_recognizer.recognized_text)
+                cr.speak(response,"ko")
+                speech_recognizer.recognized_text = None
+            # else :
+            #     print("No text")
+            time.sleep(0.1)
+            # speech_recognizer.trigger = False
+        pass
 
-question = cr.google_free(micindex)
-gpt_response = ask_gpt(question)
-print("GPT Answer:", gpt_response)
-cr.speak(gpt_response,"ko")
+except KeyboardInterrupt:
+        print("Stopping speech recognition...")
+        speech_recognizer.stop_listening(wait_for_stop=True)
+        print("Speech recognition stopped.")
 
 
